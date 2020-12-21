78ecdffec6711a9e53e8d16390d0ee59d233c1ea
Aiden <dwx_job@163.com>
2020-12-21 13:59:02 +0800

    2/39

--- service-member/src/main/java/com/itmayiedu/controller/MemberController.java
+++ service-member/src/main/java/com/itmayiedu/controller/MemberController.java
@@ -11,14 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 	@Value("${server.port}")
 	private String serverPort;
+	private static int count = 0;
 
 	@RequestMapping("/getMemberAll")
 	public List<String> getMemberAll() {
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		count++;
 		List<String> listUser = new ArrayList<String>();
 		listUser.add("zhangsan");
 		listUser.add("lisi");
 		listUser.add("wangwu");
+		listUser.add("count" + count);
 		listUser.add("serverPort:" + serverPort);
+		System.out.println("count:" + count);
 		return listUser;
 	}
 
