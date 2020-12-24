d50a12439af81a768701f4c1da8ea14941540e1c
Aiden <dwx_job@163.com>
2020-12-24 14:42:54 +0800

    2/52

--- itmayiedu-shopp-parent/itmayiedu_shopp-weixin/src/main/java/com/itmayiedu/WeiXinApp.java
+++ itmayiedu-shopp-parent/itmayiedu_shopp-weixin/src/main/java/com/itmayiedu/WeiXinApp.java
@@ -3,9 +3,7 @@ package com.itmayiedu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.stereotype.Component;
 
-@Component(value = "me.chanjar.weixin.mp.api")
 @EnableEurekaClient
 @SpringBootApplication
 public class WeiXinApp {
