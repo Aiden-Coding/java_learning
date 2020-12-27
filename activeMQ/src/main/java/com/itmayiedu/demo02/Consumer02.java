88d93261124b1da9d473a9ef350ca8a0cd582b54
Aiden <dwx_job@163.com>
2020-12-27 20:30:13 +0800

    3/84

---
+++ activeMQ/src/main/java/com/itmayiedu/demo02/Consumer02.java
@@ -0,0 +1,67 @@
+/**
+ * 功能说明:
+ * 功能作者:
+ * 创建日期:
+ * 版权归属:每特教育|蚂蚁课堂所有 www.itmayiedu.com
+ */
+package com.itmayiedu.demo02;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/**
+ * 功能说明: <br>
+ * 创建作者:每特教育-余胜军<br>
+ * 创建时间:2018年9月25日 下午10:06:26<br>
+ * 教育机构:每特教育|蚂蚁课堂<br>
+ * 版权说明:上海每特教育科技有限公司版权所有<br>
+ * 官方网站:www.itmayiedu.com|www.meitedu.com<br>
+ * 联系方式:qq644064779<br>
+ * 注意:本内容有每特教育学员共同研发,请尊重原创版权
+ */
+public class Consumer02 {
+	// mq通讯地址
+	private static String url = "tcp://127.0.0.1:61616";
+	// 队列名称
+	private static String topicName = "my_topic";
+
+	public static void main(String[] args) throws JMSException {
+		System.out.println("我是消费者002");
+		// 1.创建连接工厂 吗，密码采用默认密码admin 和admin
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(url);
+		// 2.创建连接
+		Connection connection = factory.createConnection();
+		// 3.创建会话 参数1 设置是否需要以事务方式提交 参数2 消息方式 采用自动签收
+		connection.start();// 启动连接
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		// 4.创建目标(主题)
+		Topic topic = session.createTopic(topicName);
+		// 5.创建消费者
+		MessageConsumer consumer = session.createConsumer(topic);
+		// 6.启动监听 监听消息
+		consumer.setMessageListener(new MessageListener() {
+
+			public void onMessage(Message message) {
+				try {
+					TextMessage textMessage = (TextMessage) message;
+					System.out.println("消费者消息生产者内容:" + textMessage.getText());
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			}
+		});
+		// 不要关闭连接
+
+	}
+
+}
---
+++ springboot-producer/src/main/java/com/itmayiedu/producer/Producer.java
@@ -0,0 +1,39 @@
+/**
+ * 功能说明:
+ * 功能作者:
+ * 创建日期:
+ * 版权归属:每特教育|蚂蚁课堂所有 www.itmayiedu.com
+ */
+package com.itmayiedu.producer;
+
+import javax.jms.Queue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 功能说明: <br>
+ * 创建作者:每特教育-余胜军<br>
+ * 创建时间:2018年9月25日 下午6:22:23<br>
+ * 教育机构:每特教育|蚂蚁课堂<br>
+ * 版权说明:上海每特教育科技有限公司版权所有<br>
+ * 官方网站:www.itmayiedu.com|www.meitedu.com<br>
+ * 联系方式:qq644064779<br>
+ * 注意:本内容有每特教育学员共同研发,请尊重原创版权
+ */
+@Component
+public class Producer {
+
+	@Autowired
+	private JmsMessagingTemplate jmsMessagingTemplate;
+	@Autowired
+	private Queue queue;
+
+	@Scheduled(fixedDelay = 5000)
+	public void send() {
+		jmsMessagingTemplate.convertAndSend(queue, "生产者:" + System.currentTimeMillis());
+	}
+
+}
