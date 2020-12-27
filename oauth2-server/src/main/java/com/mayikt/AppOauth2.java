c0693dbc6357fe4cbb65b70f12367765a228241b
Aiden <dwx_job@163.com>
2020-12-28 00:14:53 +0800

    3/99

---
+++ oauth2-server/src/main/java/com/mayikt/AppOauth2.java
@@ -0,0 +1,12 @@
+package com.mayikt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOauth2 {
+	public static void main(String[] args) {
+		SpringApplication.run(AppOauth2.class, args);
+	}
+
+}
