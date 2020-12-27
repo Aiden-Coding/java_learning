4719a1d00722c769cae8676620dd3c53dae0a91d
Aiden <dwx_job@163.com>
2020-12-27 17:06:04 +0800

    3/74

---
+++ itmayiedu_springboot2.0_order_consul/src/main/java/com/itmayiedu/ConsulOrderController.java
@@ -0,0 +1,51 @@
+/**
+ * 功能说明:
+ * 功能作者:
+ * 创建日期:
+ * 版权归属:每特教育|蚂蚁课堂所有 www.itmayiedu.com
+ */
+package com.itmayiedu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 功能说明: <br>
+ * 创建作者:每特教育-余胜军<br>
+ * 创建时间:2018年9月12日 下午4:01:54<br>
+ * 教育机构:每特教育|蚂蚁课堂<br>
+ * 版权说明:上海每特教育科技有限公司版权所有<br>
+ * 官方网站:www.itmayiedu.com|www.meitedu.com<br>
+ * 联系方式:qq644064779<br>
+ * 注意:本内容有每特教育学员共同研发,请尊重原创版权
+ */
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulOrderController {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/orderToMember")
+	public String orderToMember() {
+		String memberUrl = "http://consul-member/member";
+		return restTemplate.getForObject(memberUrl, String.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulOrderController.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
