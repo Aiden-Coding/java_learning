b565d2e9aee1f4fdc49b72f8fde9d57b83c4810b
Aiden <dwx_job@163.com>
2020-12-28 11:50:39 +0800

    3/116  4

---
+++ meite-shop-parent/meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/MyCommandLineRunner.java
@@ -0,0 +1,43 @@
+package com.mayikt;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * 
+ * @description: 分布式阿波罗分布式配置中心方法监听
+ * @author: 97后互联网架构师-余胜军
+ * @contact: QQ644064779、微信yushengjun644 www.mayikt.com
+ * @date: 2019年1月3日 下午3:03:17
+ * @version V1.0
+ * @Copyright 该项目“基于SpringCloud2.x构建微服务电商项目”由每特教育|蚂蚁课堂版权所有，未经过允许的情况下，
+ *            私自分享视频和源码属于违法行为。
+ */
+@Component
+@Slf4j
+public class MyCommandLineRunner implements CommandLineRunner {
+	@ApolloConfig
+	private Config config;
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("###############MyCommandLineRunner启动#############");
+		// 阿波罗方法监听
+		config.addChangeListener(new ConfigChangeListener() {
+
+			@Override
+			public void onChange(ConfigChangeEvent changeEvent) {
+				log.info("####分布式配置中心监听#####" + changeEvent.changedKeys().toString());
+			}
+		});
+	}
+
+}
