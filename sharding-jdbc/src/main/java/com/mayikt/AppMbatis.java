a477c6943dca298bb591848cdee6a58d99df7b7e
Aiden <dwx_job@163.com>
2020-12-28 10:58:30 +0800

    3/109

---
+++ sharding-jdbc/src/main/java/com/mayikt/AppMbatis.java
@@ -0,0 +1,14 @@
+package com.mayikt;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.mayikt.mapper")
+public class AppMbatis {
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppMbatis.class, args);
+	}
+}
